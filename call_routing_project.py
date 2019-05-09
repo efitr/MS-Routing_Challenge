d1840e4af66fdcdc7df05d2c5e9d17d9b6a1d903
Egon Fiedler <efiedler93@gmail.com>
2019-05-09 14:52:23 -0700

    basic structure

--- call_routing_project.py
+++ call_routing_project.py
@@ -19,4 +19,21 @@ def cost_of_calling_one_route(numbers_list, prefix_dict):
             #  If a number coincides with a prefix, return that vuew
             if number in prefix:
                 return value
+
+def get_cost(telephone):
             
+def main():
+    # file path for the routing
+    # file path for the telephone
+
+    # TupleTrie.search(telephone_number)
+        # return the longest prefix
+
+    # def get_cost(longest_prefix, phone_number)
+        # here is the dictionary 
+        # returns the cost
+
+    # def get_phone_with_cost(telephone):
+        # Trie.search for the longest_prefix
+        # get_cost(longest_prefix, telephone_number)
+        # return tuple with phone number with routing cost
