52a0e18980ec843090577736987806279b65c635
Egon Fiedler <efiedler93@gmail.com>
2019-05-09 15:37:28 -0700

    Setup of the files, Path, File and list with items on file

--- call_routing_project.py
+++ call_routing_project.py
@@ -3,26 +3,9 @@
 ############# Call routing project
 ###########################################################################################
 
-# Scenario1: 
+# Objective
 
-def cost_of_calling_one_route(numbers_list, prefix_dict):
-
-    # Iterate through every number in the numbers list
-    for number in numbers_list:
-        # For each number get an index
-        for prefix, value in enumerate(prefix_dict):
-            
-            current_prefix = prefix_dict[0]
-
-            if len(current_prefix) > 
-
-            #  If a number coincides with a prefix, return that vuew
-            if number in prefix:
-                return value
-
-def get_cost(telephone):
-            
-def main():
+# Process
     # file path for the routing
     # file path for the telephone
 
@@ -37,3 +20,39 @@ def main():
         # Trie.search for the longest_prefix
         # get_cost(longest_prefix, telephone_number)
         # return tuple with phone number with routing cost
+
+# Scenario1: 
+
+
+# def get_cost(telephone):
+            
+def main():
+    ##### PATH
+    # Phone Number Paths
+    path_phone_number_10 = "../../../../../RoutingFiles/CallRouting/phone-numbers-10.txt"
+    # path_phone_number_100 = "../../../../../RoutingFiles/CallRouting/phone-numbers-100.txt"
+    # path_phone_number_1000 = "../../../../../RoutingFiles/CallRouting/phone-numbers-1000.txt"
+    # path_phone_number_10000 = "../../../../../RoutingFiles/CallRouting/phone-numbers-10000.txt"
+
+    # Routing Cost Paths
+    path_routing_costs_10 = "../../../../../RoutingFiles/CallRouting/route-costs-10.txt"
+    # path_routing_costs_100 = "../../../../../RoutingFiles/CallRouting/route-costs-100.txt"
+    # path_routing_costs_600 = "../../../../../RoutingFiles/CallRouting/route-costs-600.txt"
+    # path_routing_costs_35000 = "../../../../../RoutingFiles/CallRouting/route-costs-35000.txt"
+    # path_routing_costs_106000 = "../../../../../RoutingFiles/CallRouting/route-costs-106000.txt"
+    # path_routing_costs_1000000 = "../../../../../RoutingFiles/CallRouting/route-costs-1000000.txt"
+    # path_routing_costs_10000000 = "../../../../../RoutingFiles/CallRouting/route-costs-10000000.txt"
+
+    ##### FILE
+    # Phone Number File
+    phone_numbers_file = open(path_phone_number_10, 'r')
+    # Routing Cost File
+    routing_costs_file = open(path_routing_costs_10, 'r')
+    
+    phone_numbers = phone_numbers_file.readlines()
+    routing_costs = routing_costs_file.readlines()
+    print(phone_numbers)
+    print(routing_costs)
+
+if __name__ == '__main__':
+    main()
